4b57c158ba77befa8a4352334c7ef4247f57e5c0
Orudnev <olrudnev@gmail.com>
2020-12-15 17:58:25 +0600

    Add actions

---
+++ src/actions/actionStorageGetItem.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const ACTTYPE_STORECLOUD_REQUEST_GETALLITEMS = 'ACTTYPE_STORECLOUD_REQUEST_GETALLITEMS';
+export const ACTTYPE_STORECLOUD_GETALLITEMS = 'ACTTYPE_STORECLOUD_GETALLITEMS';
+
+
+export function actStoreCloud_GetAllItems()
+{
+    var url = "https://script.google.com/macros/s/AKfycbyjRdA18bzdVoNSkQyeu-mbE3CfM83Qng48ynNGziMEzINAe2I/exec?method=getAllRows";
+    return dispatch=>{
+        dispatch({
+            type: ACTTYPE_STORECLOUD_REQUEST_GETALLITEMS
+        });
+        return axios.get(url)
+        .then(response=>response.data.result)
+        .then(payload=>dispatch({
+            type:ACTTYPE_STORECLOUD_GETALLITEMS,
+            payload}));
+    }   
+}
